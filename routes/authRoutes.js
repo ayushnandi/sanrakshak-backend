506a6da670186acdd7c5426a37f7449a0a1f696c
abhay <abhaym1205@gmail.com>
2024-03-19 18:03:14 +0530

    added auth controllers login logout and register functionlity

---
+++ routes/authRoutes.js
@@ -0,0 +1,16 @@
+import express from "express";
+import register from "../controllers/Auth/register.js";
+import login from "../controllers/Auth/login.js";
+import logout from "../controllers/Auth/logout.js";
+
+const router = express.Router();
+
+router.get('/', (req, res) => res.json({ message: "Hello from the auth routes " }))
+
+router.get('/register', register);
+router.get('/login', login);
+router.get('/logout', logout);
+
+
+const authRouter = router;
+export default authRouter;
