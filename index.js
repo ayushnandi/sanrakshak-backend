4cfe712ace395886d95c834bbc2a1e2f32e951af
abhay <abhaym1205@gmail.com>
2024-03-21 19:20:09 +0530

    removed cors error

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(bodyParser.json({ limit: '35mb' }));
 // adding middleware
 app.use(
   cors({
-    origin: "http://127.0.0.1:3000",
+    origin: "http://localhost:3000",
     credentials: true,
   })
 );
